41105d2fb2a12803c38795f74922c6ea58f0811f
Jo <jordanmoret4@gmail.com>
2025-03-11 11:27:46 +0100

    error-ok

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -3,20 +3,14 @@ import PropTypes from 'prop-types';
 import './Collapse.scss';
 
 const Collapse = ({ title, content }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-
   return (
     <div className="collapse">
-      <button className={`collapse-btn ${isOpen ? 'open' : ''}`} onClick={() => setIsOpen(!isOpen)}>
+      <button className={`collapse-btn`}>
         {title}
-        <i className={`fa-solid ${isOpen ? 'fa-chevron-up' : 'fa-chevron-down'}`}></i>
       </button>
-      {isOpen && (
-        <div className={`collapse-content ${isOpen ? 'show' : ''}`}>
-          {typeof content === 'string' ? <p>{content}</p> : content}
-        </div>
-      )}
+      <div className={`collapse-content`}>
+        {typeof content === 'string' ? <p>{content}</p> : content}
+      </div>
     </div>
   );
 };
