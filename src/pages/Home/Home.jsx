13ccfffda9c05217a20b412125661beacd24eace
Jo <jordanmoret4@gmail.com>
2025-03-17 22:03:58 +0100

    fetch-ok

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,43 @@
+import { useEffect, useState } from 'react';
 import Banner from '../../components/Banner/Banner';
 import Card from '../../components/Card/Card';
 import Footer from '../../components/Footer/Footer';
-import accommodations from '../../data/accommodations.json'; // Import des données JSON
 import './Home.scss'; // Import des styles spécifiques à la page Home
 
 const Home = () => {
+  const [accommodations, setAccommodations] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchAccommodations = async () => {
+      try {
+        const response = await fetch('/src/data/accommodations.json'); 
+        if (!response.ok) {
+          throw new Error('Erreur lors du chargement des données');
+        }
+        const data = await response.json();
+        setAccommodations(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchAccommodations();
+  }, []);
+
+  if (loading) {
+    return <p>Chargement des données...</p>;
+  }
+
+  if (error) {
+    return <p>Erreur : {error}</p>;
+  }
+
   return (
     <div className="home">
-
       <main className="main-index">
         {/* Banner */}
         <Banner title="Chez vous, partout et ailleurs" />
