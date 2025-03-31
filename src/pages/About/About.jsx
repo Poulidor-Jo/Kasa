4aeaf1a5d4c8468710f1c6995fc93fd99ee334c3
Jo <jordanmoret4@gmail.com>
2025-03-31 14:27:43 +0200

    remove-index/Add-comment

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -23,20 +23,20 @@ const About = () => {
             {
               title: "Fiabilité",
               content:
-                "Les annonces postées sur Kasa garantissent une fiabilité totale...",
+                "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.",
             },
             {
               title: "Respect",
               content:
-                "La bienveillance fait partie des valeurs fondamentales de Kasa...",
+                "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
             },
             {
               title: "Service",
-              content: "Nos équipes se tiennent à votre disposition...",
+              content: "La qualité du service est au cœur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance.",
             },
             {
               title: "Sécurité",
-              content: "La sécurité est la priorité de Kasa...",
+              content: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
             },
           ].map(({ title, content},index) => (
             <Collapse key={`${title}-${index}`} title={title} content={content} />
